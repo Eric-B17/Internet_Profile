22b277f44bd8c6f5afbd80d50e6d1c4ade8e4399
Bishoeri000 <Bishoeri000@warren.k12.in.us>
2024-09-13 09:12:37 -0400

    Initial Commit

---
+++ src/Main.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val amanda = Person("Amanda", 33, "play tennis", null)
+    val atiqah = Person("Atiqah", 28, "climb", amanda)
+}
+
+class Person(
+    val name: String,
+    val age: Int,
+    val hobby: String?,
+    val referrer: Person?
+) {
+
+    fun showProfile() {
+        println("Name: $name")
+        println("Age: $age")
+        if (hobby != null) {
+            println("Likes to $hobby.")
+        }
+        if (referrer != null) {
+            println("Has a referrer name ${referrer.name}")
+        } else {
+            println(".")
+        }
+        println("/n/n")
+        ///
+    }
+}
